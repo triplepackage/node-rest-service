ef4940479c2ef343521ccba55f9461858185c07f
John Domingo <triplepackage@users.noreply.github.com>
2019-08-07 18:34:58 -0400

    Cleaning up

--- makepretty.js
+++
@@ -1,5 +0,0 @@
-var beautify = require("json-beautify");
-
-var obj = { str: "Hello World", num: 42, smallarray: [ 1, 2, 3, "foo", {} ], smallobject: { foo: "bar", bar: 42 }, bigarray: [ 1, 2, 3, "foo", { foo: "bar", bar: "42", arr: [ 1, 2, 3, "foo", {} ] } ], bigobject: { foo: [ 1, 2, 3, "foo", {} ], bar: 42, a: {b: { c: 42 }}, foobar: "FooBar" } };
-
-console.log(beautify(obj, null, 2, 80));
